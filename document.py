c775dc7f3b6a910da15660d371f96b98e06682e0
Katie <katiechangg@gmail.com>
2018-06-25 12:03:49 +0800

    commit

--- document.py
+++ document.py
@@ -8,45 +8,37 @@ client = MongoClient('mongodb://10.34.33.28:33333')
 db = client.database
 crawl = db.crawl
 
-global tublelist
+global profilelist[]
 
 class profile:
 
-    imagepath = ""
-    source = ""
-    web_url = ""
-    image_url = ""
-    datetime = 0
-    keyword =  ""
-    tag = ""
-    change = ""
-    
-
-    def create (self):
+    @staticmethod
+    def create (imagepath, source, web_url, image_url, datetime, keyword):
         prev_id = db.crawl.count()
         documentformatter={
-            "source":self.source,
-            "web url": self.web_url,
-            "image url":self.image_url,
-            "keyword":self.keyword,
-            "height":self.imagepath.shape[0],
-            "width":self.imagepath.shape[1],
-            "datetime": self.datetime,
+            "source":source,
+            "web url": web_url,
+            "image url":image_url,
+            "keyword":keyword,
+            "height":imagepath.shape[0],
+            "width":imagepath.shape[1],
+            "datetime": datetime,
             "_id" : ObjectId(repr(prev_id+1))
         }
         db.crawl.insert([documentformatter])
 
-    def find_bykeyword (self, tags):
+    @staticmethod
+    def find_bykeyword (tags):
         count = db.crawl.find({"keyword":{"$all":tags}}).count()
         if count  > 0:
             for users in count:
                 profilelist[user] = db.crawl.find_one({"keyword":{"$all":tags}}).skip(user)
-            tuplelist = tuple(profilelist)
             return db.crawl.find({"keyword":{"$all":tags}}).count()
         else:
             return -1
 
-    def update(self, tag, change):
+    @staticmethod
+    def update(tag, change):
         numupdates = find_bykeyword(tag)
         if numupdates > 0:
             db.collection.update({"keyword":tag},{"keyword":change} )
