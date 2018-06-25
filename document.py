dfc401281c3af653308da0fe6d8dd5353a4cc9c1
commanderchewbacca <commanderchewbacca@gmail.com>
2018-06-25 13:12:27 +0800

    minor changes

--- document.py
+++ document.py
@@ -4,14 +4,14 @@ from pymongo import MongoClient
 from bson import ObjectId
 import cv2
 
-client = MongoClient('mongodb://10.34.33.28:33333')
-db = client.database
-crawl = db.crawl
+
 
 global tublelist
 
 class profile:
-
+    client = MongoClient('mongodb://10.34.33.28:33333')
+    db = client.database
+    crawl = db.crawl
     imagepath = ""
     source = ""
     web_url = ""
@@ -23,7 +23,7 @@ class profile:
     
 
     def create (self):
-        prev_id = db.crawl.count()
+        prev_id = self.db.crawl.count()
         documentformatter={
             "source":self.source,
             "web url": self.web_url,
@@ -34,31 +34,31 @@ class profile:
             "datetime": self.datetime,
             "_id" : ObjectId(repr(prev_id+1))
         }
-        db.crawl.insert([documentformatter])
+        self.db.crawl.insert([documentformatter])
 
     def find_bykeyword (self, tags):
-        count = db.crawl.find({"keyword":{"$all":tags}}).count()
+        count = self.db.crawl.find({"keyword":{"$all":tags}}).count()
         if count  > 0:
             for users in count:
-                profilelist[user] = db.crawl.find_one({"keyword":{"$all":tags}}).skip(user)
-            tuplelist = tuple(profilelist)
-            return db.crawl.find({"keyword":{"$all":tags}}).count()
+                self.profilelist[self.user] = self.db.crawl.find_one({"keyword":{"$all":tags}}).skip(self.user)
+            tuplelist = tuple(self.profilelist)
+            return self.db.crawl.find({"keyword":{"$all":tags}}).count()
         else:
             return -1
 
     def update(self, tag, change):
-        numupdates = find_bykeyword(tag)
+        numupdates = self.find_bykeyword(tag)
         if numupdates > 0:
-            db.collection.update({"keyword":tag},{"keyword":change} )
+            self.db.collection.update({"keyword":tag},{"keyword":change} )
 #update keywords
 
     def delete(self, tag):
-        numdeletes = find_bykeyword(tag)
+        numdeletes = self.find_bykeyword(tag)
         if numdeletes > 0:
-            db.collection.remove({"keyword":tag})
+            self.db.collection.remove({"keyword":tag})
 
 test = profile()
-test.imagepath = "\home\katiechang\Documents\"
+test.imagepath = print("\home\katiechang\Documents\\")
 
 
         
